84ca4fbebf2312a20f5bb2a7998a95a64579de2f
oscaroox <oscar_rank_46@hotmail.com>
2014-09-19 10:09:42 +0200

    Database class

--- core/database.php
+++ core/database.php
@@ -1,5 +1,34 @@
 <?
 
+class database{
+	
+	public $stmt;
+	public $host;
+	public $dbName;
+	public $user;
+	public $pass;
+
+
+
+
+
+
+
+	public function __construct()
+	{
+		$this->host 	= "Localhost";
+		$this->user 	= "tmtk-11";
+		$this->pass 	= "ySAbNX";
+		$this->dbName   = "dbtmtk_11";
+		$db = new PDO("mysql:");
+	}
+	
+
+
+
+
+
+}
 
 
 
