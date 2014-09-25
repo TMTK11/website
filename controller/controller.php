2c120e81354b6c9acb3fec339e09936234437af1
Miesvanderlippe <miesvanderlippe@gmail.com>
2014-09-25 13:10:13 +0200

    Better controllercheck
    Also added some comments

--- controller/controller.php
+++ controller/controller.php
@@ -3,36 +3,41 @@
 require_once("/controller/errorController.php");
 
 class Controller{
-
+	
+	//Controller vars
 	private $url;
 	private $state;
 	private $pageController;
 
+	//Constructor
 	public function __construct($url){
-
+		
 		$this->url = $url;
-
 	}
 
 	//Runs the controller creating the page
 	public function start(){
-
 		$type	 =	$this->url[0];
-
-		if(!isset($type) || null == $type){
-
+		
+		//If no page is specified user is redirected to home
+		if(!isset($type) || null == $type)
 			$type	 =	"home";
-		}
 
-		if(include("/controller/" . $type . "Controller.php")){
+		//Scan all controllers
+		$controllers = scandir("controller");
+		
+		unset($controllers['0']); //Removes .
+		unset($controllers['1']); //Removes ..
+		
+		//Check if specified controller exists
+		if(in_array($type . "Controller.php", $controllers)){
+			
+			require_once($type . "Controller.php");
 			
 			//Page logic here
 			$controllerName = ucfirst($type) . "Controller";
 			
 			$this->pageController = new $controllerName($this->url);
-
-			//Check if plugin is active
-
 
 			//Set the state
 			$this->state = "DONE";
@@ -46,16 +51,19 @@ class Controller{
 
 	}
 
+	//HTML output
 	public function getHTML(){
 
+		//Check for state
 		if("DONE" != $this->state){
 
+			//Get errorpage from errorcontroller
 			$errorController = new errorController(array("title" => "Fout!", "message"=>"Pagina kon niet worden gevonden" ));
 
-			return($errorController->getHTML(array("title" => "e")));
+			return($errorController->getHTML());
 
 		}else{
-		
+			//Gets HTML from pagecontroller and return it to index.php
 			$html = $this->pageController->getHTML();
 			
 			return($html);
