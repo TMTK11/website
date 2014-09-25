96336365fa0a5b4c6052d9bddd8124c40bea2ee4
Selle <michaeldejong26@gmail.com>
2014-09-25 14:30:33 +0200

    backendView Settings

--- view/backendView.php
+++ view/backendView.php
@@ -68,7 +68,7 @@ class backendView{
 				<div class=\"navbar-inner\">
 						<ul class=\"nav pull-right\">
 							
-							<li><a href=\"#\" class=\"hidden-phone visible-tablet visible-desktop\" role=\"button\">Settings</a></li>
+							<li><a href=\"settings\" class=\"hidden-phone visible-tablet visible-desktop\" role=\"button\">Settings</a></li>
 							<li id=\"fat-menu\" class=\"dropdown\">
 								<a href=\"#\" role=\"button\" class=\"dropdown-toggle\" data-toggle=\"dropdown\">
 									<i class=\"icon-user\"></i> Jack Smith
@@ -78,7 +78,7 @@ class backendView{
 								<ul class=\"dropdown-menu\">
 									<li><a tabindex=\"-1\" href=\"#\">My Account</a></li>
 									<li class=\"divider\"></li>
-									<li><a tabindex=\"-1\" class=\"visible-phone\" href=\"#\">Settings</a></li>
+									<li><a tabindex=\"-1\" class=\"visible-phone\" href=\"home\">Settings</a></li>
 									<li class=\"divider visible-phone\"></li>
 									<li><a tabindex=\"-1\" href=\"sign-in.html\">Logout</a></li>
 								</ul>
