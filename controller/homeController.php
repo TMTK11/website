7b6d10f5f2979b845740a83870ca2669fc7d92c4
Oscar Druiventak <oscar_rank_46@hotmail.com>
2014-10-09 14:50:37 +0200

    small changes

--- controller/homeController.php
+++ controller/homeController.php
@@ -4,22 +4,44 @@ require_once("/controller/layoutController.php");
 
 class HomeController{
 	
-	function __construct(){
+	private $url;
+
+	function __construct($url){
+
+		$this->url = $url;
+
+		if($url[1] == 'login'){
+			//Login
+			
+		}
 
 		return(true);
 	}
 	
 	public function getHTML(){
 		
-		$message	 =	"<h1>Home</h1>";
-		$message	.=	"<p>Welkom op de pagina voor TMTK1-11. Hier maken wij ons MVC CMS.</p>";
-		$title		 =	"Home ";
+		if($url[1] == login  && $loginsucces){
+
+			$hmtl = "login succesvol"; 
+
+		}elseif($url[1] == login  && !$loginsucces){
 		
-		$details = array("contentLeft"=>$message, "title"=>$title);
+			$errorController = new errorController(array("title" => "Foute inlog!", "message"=>"Verkeerde gebruikersnaam of wachtwoord." ));
+			$html = $errorController->getHTML();
 		
-		$layout	 =	new LayoutController($details);
-		
-		return($layout->getHTML());
+		}else{
+
+			$message	 =	"<h1>Home</h1>";
+			$message	.=	"<p>Welkom op de pagina voor TMTK1-11. Hier maken wij ons MVC CMS.</p>";
+			$title		 =	"Home ";
+			
+			$details = array("contentLeft"=>$message, "title"=>$title);
+			
+			$layout	 =	new LayoutController($details);
+			
+			$html = $layout->getHTML();
+		}
+		return($html);
 	}
 }
 
