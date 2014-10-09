93628f2bc41b3c1194dacb3c157a8a1f01b50b1d
Miesvanderlippe <miesvanderlippe@gmail.com>
2014-10-09 15:40:23 +0200

    Deleted two useless files from root

--- connection.php
+++
@@ -1,9 +0,0 @@
-<?php
-// -- Made By ICT-LAB
-$host = "127.0.0.1";
-$gebruiker_mysql = "tmtk-11";
-$wachtwoord2 = "ySAbNX";
-$DBNaam = "dbtmtk_11";
-$Verbinding = mysql_connect("$host", "$gebruiker_mysql", "$wachtwoord2") or die("<p>De verbinding met de database kan niet worden gemaakt</p>" . mysql_error());
-mysql_select_db($DBNaam) or die("<p>De database kan niet geselecteerd worden</p>");
-?>
